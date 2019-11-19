fe67bd4cda665e3a52f81d1d01a46848d710bed0
John Chapple <johnchapple@win.cis.ksu.edu>
2019-11-19 15:16:31 -0600

    3:16PM

--- Movies/Pages/Index.cshtml.cs
+++ Movies/Pages/Index.cshtml.cs
@@ -9,10 +9,35 @@ namespace Movies.Pages
 {
     public class IndexModel : PageModel
     {
-
+        public MovieDatabase mdb = new MovieDatabase();
+        public List<Movie> Movies;
         public void OnGet()
         {
+            Movies = mdb.All;
+        }
 
+        public float minIMDB;
+
+        public void OnPost(string search, List<string> mpaa)
+        {
+            if ((search != null) && (mpaa.Count > 0))
+            {
+                Movies = mdb.Search(search);
+                Movies = mdb.Filter(Movies, mpaa);
+            }
+            else if (search != null)
+            {
+                Movies = mdb.Search(search);
+            }
+            else if (mpaa.Count > 0)
+            {
+                Movies = mdb.Filter(mdb.All, mpaa);
+            }
+
+            if (minIMDB is float min)
+            {
+                Movies = mdb.FilterByMinIMDB(Movies, min);
+            }
         }
     }
 }

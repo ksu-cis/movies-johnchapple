f593acb19eb76ce52ccefbf2cf260c03279ef383
John Chapple <johnchapple@win.cis.ksu.edu>
2019-11-21 15:25:27 -0600

    ajax

--- Movies/wwwroot/js/site.js
+++ Movies/wwwroot/js/site.js
@@ -2,3 +2,36 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your Javascript code.
+
+var form = document.getElementById("form")
+
+form.addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    var inputs = document.getElementsByName("input")
+    var formData = new FormData()
+    for (var i = 0; i < inputs.length; i++) {
+        if (inputs[i].type == 'checkbox') {
+            if (inputs[i].checked) { formData.append(inputs[i].name, inputs[i].value) }
+        } else {
+            formData.set(inputs[i].name, inputs[i].value)
+        }
+        if (inputs[i].name == "__RequestVerificationToken") {
+            token = inputs[i].value;
+        }
+        
+    }
+
+
+
+    var xhr = new XMLHttpRequest();
+    xhr.open('POST', '/', true);
+    xhr.setRequestHeader("RequestVerificationToken", token);
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === XMLHttpRequest.DONE && xhr.status == 200) {
+            console.log(xhr.responseText)
+        }
+    }
+    xhr.send(formData);
+
+})
